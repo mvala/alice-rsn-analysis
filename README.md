75dd33e240b365fe11f0659de6c3659cbca63113
Martin Vala <mvala@saske.sk>
2016-02-26 09:03:01 +0100

    alice_cp script added

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# alice-rsn-analysis
+# Copy data from AliEn to ALIKE EOS
+
+## Create AliEn token (it is valid for 24 hours)
+
+```
+$ alien-token-init
+```
+
+## Create list of files to be downloaded
+
+This example will show LHC15f pass2 data for run 225000
+```
+aliensh
+aliensh:[alice] [2] /alice/cern.ch/user/m/mvala/ > find  /alice/data/2015/LHC15f/000225000/pass2 AliESDs.root > LHC15f_pass2_225000.txt
+```
+
+## Download files
+```
+${ALI_RSN_ANALYSUS_PROJECT_DIR}/scripts/alice_cp.sh LHC15f_pass2_225000.txt
+```
---
+++ scripts/alice_cp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+trap '{ echo "Hey, you pressed Ctrl-C.  Time to quit." ; exit 1; }' INT
+
+OUT_EOS=/eos/alike.saske.sk
+OUT_ARCHIVE=${2:-root_archive.zip}
+function help() {
+	echo "Usage : $0 <filename>"
+}
+
+[ -n "$1" ] || { help; exit 1; }
+[ -f "$1" ] || { echo "Error : file $1 doesn't exist" ; exit 2; }
+
+alien-token-info
+[ $? -eq 0 ] || { echo "Error : AliEn token is not valid!!! Do 'alien-token-init' first ..." ; exit 2;}
+
+while read -r line; do
+    name="$line"
+    [[ $name == /alice* ]] || continue
+    base_dir=${line/AliESDs.root/}
+    [ -f $OUT_EOS/$base_dir/$OUT_ARCHIVE ]&& { echo "Skipping '$OUT_EOS$base_dir$OUT_ARCHIVE' ..."; continue; }
+    mkdir -p $OUT_EOS$base_dir
+    alien_cp alien://$base_dir$OUT_ARCHIVE $OUT_EOS/$base_dir | grep -v Overriding
+done < "$1"
