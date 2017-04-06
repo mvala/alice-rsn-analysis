d1d866ef5879511e1a889001b0a9406fa5b80720
Martin Vala <mvala@saske.sk>
2017-04-06 09:26:27 +0200

    Skeleton for analysis task

--- macros/train/AddTaskRsnPhiSpinPol.C
+++ macros/train/AddTaskRsnPhiSpinPol.C
@@ -1,5 +1,6 @@
 AliRsnMiniAnalysisTask *AddTaskRsnPhiSpinPol(TString name = "PhiKK_SP",
-                                             Bool_t isMC) {
+                                             Bool_t isMC,
+                                             TString outputFileName = "") {
 
   // Retrieve global Analysis Manager
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
@@ -8,7 +9,30 @@ AliRsnMiniAnalysisTask *AddTaskRsnPhiSpinPol(TString name = "PhiKK_SP",
     return NULL;
   }
 
+  // Creating Rsn Task
   AliRsnMiniAnalysisTask *task = new AliRsnMiniAnalysisTask(name.Data(), isMC);
 
+  // ==== START RSN CONFIG =========
+
+  
+  // ====== END RSN CONFIG =========
+
+  // Contecting input container (ESD or AOD) from Analysis Manager
+  mgr->ConnectInput(task, 0, mgr->GetCommonInputContainer());
+
+  // Setting ouptut filename
+  if (outputFileName.IsNull())
+    outputFileName = AliAnalysisManager::GetCommonFileName();
+
+  TString outListName = "RsnOutput";
+  // Creating output container
+  AliAnalysisDataContainer *output = mgr->CreateContainer(
+      outListName.Data(), TList::Class(), AliAnalysisManager::kOutputContainer,
+      outputFileName.Data());
+
+  // Connecting output
+  mgr->ConnectOutput(task, 1, output);
+
+  // returning task
   return task;
 }
